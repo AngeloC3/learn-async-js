4d5d3e7be0578bf4e16ba9f692973f2a8eb20b01
AngeloC <cataldo.an@northeastern.edu>
2024-02-27 13:20:29 -0500

    feb 27

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,28 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function sumOfArow(arr, rowIdx) {
+    return new Promise((resolve, reject) => {
+        if (arr.length > rowIdx) {
+            setTimeout(() => {
+                arr[rowIdx].filter((e) => {
+                    return e < 0;
+                }).length > 0 ? resolve(`Found row: ${arr[rowIdx]}`) : reject("Not")
+            }, 0);
+        } else {
+            reject("ERROR!");
+        }
+    })
+}
+
+promises = [];
+for (let x = 0; x < array2D.length; x++) {
+    promises.push(sumOfArow(array2D, x));
+}
+
+Promise.any(promises)
+    .then((results) => {
+        console.log(results);
+    })
+    .catch((error) => console.log("Error: " + error));
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,34 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function sumOfArow(arr, rowIdx) {
+    return new Promise((resolve, reject) => {
+        if (arr.length > rowIdx) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr[rowIdx].length; i++) {
+                    sum += arr[rowIdx][i];
+                }
+                resolve(sum);
+            }, 0);
+        } else {
+            reject("ERROR!");
+        }
+    })
+}
+
+promises = [];
+for (let x = 0; x < array2D.length; x++) {
+    promises.push(sumOfArow(array2D, x));
+}
+
+Promise.all(promises)
+    .then((rowSums) => {
+        let sum = 0;
+        rowSums.forEach(rowSum => {
+            sum += rowSum;
+        })
+        console.log("Sum = " + sum);
+    })
+    .catch((error) => console.log("Error: " + error));
