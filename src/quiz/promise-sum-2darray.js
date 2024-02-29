37ec8266e389f30942e3d5ce4e096717c00efccb
AngeloC <cataldo.an@northeastern.edu>
2024-02-29 15:31:30 -0500

    feb 29

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,38 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function sumOfArow(arr, rowIdx) {
+    return new Promise((resolve, reject) => {
+        if (arr.length > rowIdx) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr[rowIdx].length; i++) {
+                    sum += arr[rowIdx][i];
+                }
+                resolve(sum);
+            }, 0);
+        } else {
+            reject("ERROR!");
+        }
+    })
+}
+
+async function calcSum() {
+    promises = [];
+    for (let x = 0; x < array2D.length; x++) {
+        promises.push(sumOfArow(array2D, x));
+    }
+    try {
+        const rowSums = await Promise.all(promises);
+        let sum = 0;
+        rowSums.forEach(rowSum => {
+            sum += rowSum;
+        })
+        console.log("Sum = " + sum);
+    } catch (error) {
+        console.log("Error: " + error);
+    }
+}
+
+calcSum();
